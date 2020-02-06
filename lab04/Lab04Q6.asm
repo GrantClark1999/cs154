5b966316eb927558dbade364ce0cc9c84aa0b347
Grant Clark <grantclark1999@gmail.com>
2020-02-05 23:20:51 -0800

    Lab04Q6.asm

--- lab04/Lab04Q6.asm
+++ lab04/Lab04Q6.asm
@@ -16,30 +16,30 @@ main:
     move $a0, $s0
     syscall
 
-#     addi $s1, $zero, 0x14       # $s1 = 20 (const, loop stop condition)
-#     la $s2, array               # $s2 = a
+    addi $s1, $zero, 0x14       # $s1 = 20 (const, loop stop condition)
+    la $s2, array               # $s2 = a
 
-# loop:
-#     addi $s0, $s0, 0x4
-#     beq $s0, $s1, exit
-#     add $v0, $zero, $zero       # $v0 = 0
+loop:
+    addi $s0, $s0, 0x4
+    beq $s0, $s1, exit
+    add $v0, $zero, $zero       # $v0 = 0
 
-#     add $t0, $s2, $s0           # $t0 = &a[i]
-#     lw $a0, 0($t0)              # $a0 = a[i]
-#     slt $t1, $a1, $a0           # (b < a[i]) ? 1 : 0
-#     beq $t1, $zero, print       # branch when a[i] <= b
+    add $t0, $s2, $s0           # $t0 = &a[i]
+    lw $a0, 0($t0)              # $a0 = a[i]
+    slt $t1, $a1, $a0           # (b < a[i]) ? 1 : 0
+    beq $t1, $zero, print       # branch when a[i] <= b
 
-#     jal rick
+    jal rick
 
-# print:
-#     add $a0, $v0, $zero         # print v
-#     addi $v0, $zero, 0x1
-#     syscall
-#     la $a0, units               # print string
-#     addi $v0, $zero, 0x4
-#     syscall
+print:
+    add $a0, $v0, $zero         # print v
+    addi $v0, $zero, 0x1
+    syscall
+    la $a0, units               # print string
+    addi $v0, $zero, 0x4
+    syscall
 
-#     j loop
+    j loop
 
 exit:
     addi $v0, $zero, 0xA         # exit syscall
